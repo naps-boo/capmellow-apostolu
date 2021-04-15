ef873e39f5bd07a1735448206274ba56c906ee4b
naps-boo <natapostolu@gmail.com>
2021-04-14 22:55:55 -0300

    app creada

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+    return(
+       <div classname="App" >
+        <ul><li><a href="#acerca">Acerca</a></li></ul>
+        <ul><li><a href="#contacto">Contacto</a></li></ul>
+        <ul><li><a href="#historial">Historial</a></li></ul>
+        <ul><li><a href="#ayuda">Ayuda</a></li></ul>
+        <ul><li><a href="#categoria">Categoria</a></li></ul>
+        </div>     
+   )
+
+}
+export default NavBar;
